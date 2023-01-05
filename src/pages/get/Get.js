670ab103ad45be20b0694e7ad8b254ff4d164127
MajidovDoniyor <majidov.doniyor007@gmail.com>
2023-01-05 10:58:29 +0500

    get

--- src/pages/get/Get.js
+++ src/pages/get/Get.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Get = () => {
+  const [state, setstate] = useState([]);
+  const [page, setpage] = useState(1);
+
+  useEffect(() => {
+    fetch(`http://localhost:8888/employees?_limit=2&_page=${page}`)
+      .then((res) => res.json())
+      .then((res) => setstate(res));
+  }, [page]);
   return (
-    <div>
-      <h1>get</h1>
+    <div className="container">
+      <button className="btn btn-success" onClick={() => setpage(1)}>
+        1
+      </button>
+      <button className="btn btn-success" onClick={() => setpage(2)}>
+        2
+      </button>
+      {state?.map((item) => {
+        return <h1 key={item.id}>{item.name}</h1>;
+      })}
     </div>
   );
 };
